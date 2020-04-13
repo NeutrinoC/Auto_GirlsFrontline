fe850ed37d6820511a1205de1dea3f2cab0728f8
NeutrinoC <35862168+NeutrinoC@users.noreply.github.com>
2020-04-13 19:21:48 +0800

    Add files via upload

--- autoLSupport/autoLSupport.py
+++ autoLSupport/autoLSupport.py
@@ -22,42 +22,45 @@ from skimage.metrics import structural_similarity
 #=============================================#
 
 
+
 #=================截图比对区域=================#
 IMAGE_PATH = 'initial_IMG/'#读取截图的路径
-MAIN_MENU_IMAGE_BOX = [0.62,0.52,0.76,0.60]#主界面判断区域                      
-L_SUPPORT_IMAGE_BOX = [0.02,0.30,0.18,0.37]#后勤完成界面判断区域         
-DESKTOP_IMAGE_BOX = [0.10,0.20,0.22,0.40]#模拟器桌面判断区域         
+MAIN_MENU_IMAGE_BOX = [0.65,0.52,0.75,0.58]#主界面判断区域                       
+L_SUPPORT_IMAGE_BOX = [0.05,0.30,0.18,0.39]#后勤完成界面判断区域                       
+DESKTOP_IMAGE_BOX = [0.10,0.20,0.22,0.35]#模拟器桌面判断区域         
 
 #=================点击拖动区域=================#
+
 #收后勤支援
 L_SUPPORT_STEP1_CLICK_BOX = [0.50,0.50,0.60,0.60]#确认后勤完成
-L_SUPPORT_STEP2_CLICK_BOX = [0.53,0.62,0.62,0.67]#再次派出
+L_SUPPORT_STEP2_CLICK_BOX = [0.53,0.60,0.62,0.65]#再次派出
 
 #启动游戏
 START_GAME_STEP1_CLICK_BOX = [0.14,0.23,0.18,0.28]#点击图标启动
-START_GAME_STEP2_CLICK_BOX = [0.50,0.70,0.50,0.70]#点击开始
-START_GAME_STEP3_CLICK_BOX = [0.50,0.75,0.50,0.75]#点击登录
- 
+START_GAME_STEP2_CLICK_BOX = [0.50,0.70,0.50,0.70]#点击一次
+START_GAME_STEP3_CLICK_BOX = [0.50,0.75,0.50,0.75]#点击开始 
+
 #关闭游戏
-CLOSE_GAME_CLICK_BOX = [0.52,0.03,0.53,0.04]
+CLOSE_GAME_CLICK_BOX = [0.56,0.02,0.57,0.04]
+
+
 #=============================================#
 #                                             #
 #                 基本功能函数                 #
 #                                             #
 #=============================================#
 
-#启动界面，传统艺能
+#一个好程序都应该有一个较为优雅的启动提醒界面？
 def preface():    
-    print(">>>这是一个自动收派后勤的程序，每5~6分钟检测一次\n")
-    for x in range(5,-1,-1):
-        mystr =">>> 将在"+str(x)+"s 后启动"
+    for x in range(3,-1,-1):
+        mystr =">>> "+str(x)+"s 后将开始操作，请切换至模拟器界面"
         print(mystr,end="")
         print("\b" * (len(mystr)*2),end = "",flush=True)
         time.sleep(1)
     print(">>> 开始操作,现在是",datetime.datetime.now(),"\n")
 
 
-#等待若干时间,时间在min~max之间
+#随机等待一段时间,控制在minTime~maxTime之间
 def wait(minTime,maxTime):
     waitTime = minTime + (maxTime - minTime) * random.random()
     time.sleep(waitTime)
@@ -79,28 +82,32 @@ def getWindowData():
     width  = right - left
     height = bottom - top
     return [left,top,right,bottom,width,height]
-        
+
 
 #获取指定区域box的截图
 def getImage(box):
     #windowData = [left,top,right,bottom,width,height]        
     windowData = getWindowData()
-    imgLeft   = int(windowData[0] + windowData[4] * box[0])
-    imgTop    = int(windowData[1] + windowData[5] * box[1])
-    imgRight  = int(windowData[0] + windowData[4] * box[2])
-    imgBottom = int(windowData[1] + windowData[5] * box[3])
+    imgLeft   = windowData[0] + int(windowData[4] * box[0])
+    imgTop    = windowData[1] + int(windowData[5] * box[1])
+    imgRight  = windowData[0] + int(windowData[4] * box[2])
+    imgBottom = windowData[1] + int(windowData[5] * box[3])
     img = ImageGrab.grab((imgLeft,imgTop,imgRight,imgBottom))
     return img
     
     
-#点击box内随机一点
-def mouseClick(box,minTime,maxTime):
+#点击box内随机一点，如果提供具体xy偏量，则点击精确的点
+def mouseClick(box,minTime,maxTime,exact_x = 0,exact_y = 0):
     #box = [left,top,right,bottom]
     windowData = getWindowData()
     width  = box[2] - box[0]
     height = box[3] - box[1]
-    clickX = (int)(windowData[0] + windowData[4] * box[0] + windowData[4] * width  * random.random())
-    clickY = (int)(windowData[1] + windowData[5] * box[1] + windowData[5] * height * random.random())
+    if exact_x == 0 and exact_y == 0:
+        clickX = windowData[0] + (int)(windowData[4] * box[0] + windowData[4] * width  * random.random())
+        clickY = windowData[1] + (int)(windowData[5] * box[1] + windowData[5] * height * random.random())
+    else:
+        clickX = windowData[0] + (int)(windowData[4] * box[0]) + exact_x
+        clickY = windowData[1] + (int)(windowData[5] * box[1]) + exact_y
     clickPos = (clickX,clickY)
     win32api.SetCursorPos(clickPos)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0,0,0)
@@ -108,7 +115,49 @@ def mouseClick(box,minTime,maxTime):
     wait(minTime,maxTime)
 
 
-#比较两图片吻合度，结构相似性比较法
+#模拟鼠标拖动，box为起始区域,times为拖动次数,distance为单次拖动距离
+#dx,dy为组成移动方向向量，frame_interval为鼠标拖动帧间隔,越小鼠标拖动越快
+#multi_interval为连续拖动时的时间间隔
+def mouseDrag(box,dx,dy,times,distance,frame_interval,multi_interval):
+    windowData = getWindowData()
+    width  = box[2] - box[0]
+    height = box[3] - box[1]
+    for i in range(times):
+        dragX = windowData[0] + int(windowData[4] * box[0] + windowData[4] * width  * random.random())
+        dragY = windowData[1] + int(windowData[5] * box[1] + windowData[5] * height * random.random())
+        dragPos = (dragX, dragY)
+        win32api.SetCursorPos(dragPos)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0,0,0)
+        for i in range(distance):
+            win32api.mouse_event(win32con.MOUSEEVENTF_MOVE,dx,dy,0,0)
+            time.sleep(frame_interval)
+        time.sleep(0.2)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0,0,0)
+        time.sleep(multi_interval)
+
+
+#模拟Ctrl和滚轮实现缩放地图
+#direct = 0 : 放大      direct = 1 : 缩小   times为连续缩放次数
+def scaleMap(box,direct,times):
+    windowData = getWindowData()
+    width  = box[2] - box[0]
+    height = box[3] - box[1]
+    scaleX = windowData[0] + int(windowData[4] * box[0] + windowData[4] * width  * random.random())
+    scaleY = windowData[1] + int(windowData[5] * box[1] + windowData[5] * height * random.random())
+    scalePos = (scaleX, scaleY)
+    win32api.SetCursorPos(scalePos)
+    win32api.keybd_event(0x11,0,0,0)#按下Ctrl键
+    for i in range(times):
+        if direct == 0:
+            win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL,0,0,1)
+        else:
+            win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL,0,0,-1)
+        wait(0.5,0.7)
+    win32api.keybd_event(0x11, 0, win32con.KEYEVENTF_KEYUP,0)    
+    time.sleep(1)
+        
+
+#比较两图片吻合度，结构相似性比较法（真的好用）
 def imageCompare(img1,img2):
     gray_img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     gray_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
@@ -146,14 +195,13 @@ def hideWindow():
     elif hwnd_desktop != 0:
         win32gui.ShowWindow(hwnd_desktop,win32con.SW_HIDE)
     time.sleep(3)
-
 #=============================================#
 #                                             #
 #                 高级功能函数                 #
 #                                             #
 #=============================================#
-
-#判断是否是委托完成界面
+    
+#判断是否是主界面
 def isMainMenu():
     initImage = cv2.imread(IMAGE_PATH+"main_menu.png")
     capImage  = getImage(MAIN_MENU_IMAGE_BOX)
@@ -176,18 +224,23 @@ def isDesktop():
 
 #收后勤支援
 def takeLSupport():
+    print("ACTION: 收派后勤")
     mouseClick(L_SUPPORT_STEP1_CLICK_BOX,2,3)
-    mouseClick(L_SUPPORT_STEP2_CLICK_BOX,6,8)
+    mouseClick(L_SUPPORT_STEP2_CLICK_BOX,4,5)
 
 #启动游戏
 def startGame():
+    print("ACTION: 启动游戏")
     mouseClick(START_GAME_STEP1_CLICK_BOX,30,30)
     mouseClick(START_GAME_STEP2_CLICK_BOX,30,30)
     mouseClick(START_GAME_STEP3_CLICK_BOX,30,30)
 
+
 #关闭游戏
 def closeGame():
+    print("ACTION: 关闭游戏")
     mouseClick(CLOSE_GAME_CLICK_BOX,5,5)
+
 
 #=============================================#
 #                                             #
@@ -196,6 +249,7 @@ def closeGame():
 #=============================================#
 
 if __name__ == "__main__": 
+
     preface()
     L_SupportCount = 0
     failCount = 0
@@ -226,6 +280,3 @@ if __name__ == "__main__":
             continue
         hideWindow()#隐藏窗口，这样你就可以同时做其他事了
         wait(300,360)
-                
-            
-            
