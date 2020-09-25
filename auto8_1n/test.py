237a2bd5f8df63bf1439242f608c9a77d8b01b1b
NeutrinoC <35862168+NeutrinoC@users.noreply.github.com>
2020-09-25 10:53:38 +0800

    Add files via upload

--- auto8_1n/test.py
+++ auto8_1n/test.py
@@ -1,9 +1,9 @@
 from auto8_1n import *
 
 time.sleep(0.5)
-img = getImage( [0.05,0.18,0.10,0.22])
+img = getImage([0.10,0.20,0.22,0.35])
 img.show()
-#img.save("initial_IMG/.png")
+img.save("initial_IMG/desktop.png")
 
 
 
