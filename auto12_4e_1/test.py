db0bdd5185d33ecd61f5616156337bec58b12234
NeutrinoC <35862168+NeutrinoC@users.noreply.github.com>
2020-11-19 22:58:53 +0800

    Add files via upload

--- auto12_4e_1/test.py
+++ auto12_4e_1/test.py
@@ -1,6 +1,6 @@
 from auto12_4e_1 import *
 
 time.sleep(0.5)
-img = getImage([0.38,0.28,0.40,0.32])
+img = getImage([0.05,0.20,0.10,0.24])
 img.show()
-#img.save("initial_IMG/xxx.png")
+#img.save("initial_IMG/combat_menu.png")
