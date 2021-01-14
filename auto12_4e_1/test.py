41aa9d5421909747c107d25a0178429850cdd913
NeutrinoC <35862168+NeutrinoC@users.noreply.github.com>
2021-01-14 23:58:08 +0800

    Add files via upload

--- auto12_4e_1/test.py
+++ auto12_4e_1/test.py
@@ -1,6 +1,6 @@
 from auto12_4e_1 import *
 
 time.sleep(0.5)
-img = getImage([0.05,0.28,0.10,0.32])
+img = getImage([0.05,0.20,0.10,0.24])
 img.show()
 #img.save("initial_IMG/main_menu.png")
--- auto8_1n/test.py
+++ auto8_1n/test.py
@@ -1,7 +1,7 @@
 from auto8_1n import *
 
 time.sleep(0.5)
-img = getImage([0.32,0.28,0.34,0.32])
+img = getImage([0.32,0.28,0.33,0.32])
 img.show()
 #img.save("initial_IMG/first_login.png")
 
